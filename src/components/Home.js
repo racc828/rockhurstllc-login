f22dd2af071f52d9b7cb29a6a93eb6b5e1acafa6
github username <github email address>
2017-12-29 11:46:50 -0500

    docs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import document1 from '../pdf/document1.pdf'
+import document2 from '../pdf/document2.pdf'
+import document3 from '../pdf/document3.pdf'
 
 export default class Home extends React.Component {
   constructor() {
@@ -21,6 +24,9 @@ export default class Home extends React.Component {
         <div className="container">
           <div className="main-body">
             <h5>Welcome {this.props.currentUser.firstname}</h5>
+            <a href={document1}> BLS Investors Fund II unit Purchase Agreement and Amended and Restated Limited Liabnility Company Agreement</a> <br/>
+            <a href={document2}> Rockhurst LLC Cover Letter and Schedule</a> <br/>
+            <a href={document3}>Signature Pages, Accredited Investor Questionnaire and Investor Contact Information Form</a>
           </div>
         </div>
       </div>
