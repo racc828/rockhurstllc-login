756dcbfc31471e9cf724b132d465199db773fbdd
github username <github email address>
2018-02-11 15:37:03 -0500

    last changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -76,23 +76,7 @@ export default class Login extends React.Component {
         </div>
       </div>
     </div>
-        {/* <div className="container login-container">
-            <div className="login-box">
-              <div className="z-depth-3">
-                <h5 className="center-align"> Login </h5>
-                <form className="login-form" onSubmit={this.handleSubmit}>
-                  <input name="email" type="email" placeholder="email" onChange={this.handleChange} />
-                  <input name="password" type="password" placeholder="password" onChange={this.handleChange} />
-                  <div className="right-align">
-                    <button className="waves-effect waves-light btn" type="submit">Sign In</button>
-                  </div>
-                </form>
-                {this.props.error ?
-                  <p className="error">Invalid email or password </p> : null
-                }
-              </div>
-            </div>
-        </div> */}
+        
       </div>
     )
   }
